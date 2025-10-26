370d0d0523d9b035900a3b65f4db8201da972f0e
OUANDJI Brayann <brayann.ouandji@groupe-esigelec.org>
2025-10-26 21:00:38 +0100

    modification connexion bdd

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 // Paramètres de connexion à la base de données bdd_7_5
 
 $host = 'localhost';
-$login = 'root';
-$password = '';
+$login = 'grp_7_5';
+$password = 'T0noTMCBcQKwOP';
 $dbname = 'bdd_7_5';
 ?>
--- database.php
+++ database.php
@@ -9,14 +9,13 @@ $mysqli = new mysqli($host, $login, $password, $dbname);
 
 // Vérification de la connexion
 if ($mysqli->connect_errno) {
-    // Échec de la connexion [cite: 1692, 1693]
+    
     echo "Échec de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    // En production, n'affichez pas les détails de l'erreur
-    // die("Erreur de connexion à la base de données.");
+    
     exit();
 }
 
-// Optionnel : S'assurer que les échanges se font en UTF-8
+
 if (!$mysqli->set_charset("utf8")) {
     printf("Erreur lors du chargement du jeu de caractères utf8 : %s\n", $mysqli->error);
     exit();
